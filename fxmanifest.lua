99c228da16d858ba4b201bb0b9b9083d79c99b98
Mike | iceline.host <39988281+mike-iceline@users.noreply.github.com>
2020-11-25 10:19:21 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'fendretti'
+description "EUP UI for ESX edited by theyellowguy"
+
+client_scripts {
+    '@NativeUI/NativeUI.lua',
+    'eup_ui.lua'
+}
